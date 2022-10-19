1c4caeb5bbd62ef939fc3c4972b73837717a9d64
Gustavo Goncalves <gustavo.silveira.goncalves@gmail.com>
2022-10-19 18:29:16 -0300

    Initial commit

---
+++ organizacao.js
@@ -0,0 +1,8 @@
+console.log("Linha de Codigo.");
+
+{
+    {
+        console.log("Teste");
+        console.log('Teste 2')
+    }
+}
---
+++ variaveisEConstantes.js
@@ -0,0 +1,13 @@
+let a = 3;
+let b = 5;
+
+console.log(a,b);
+
+a = 300;
+b = 500;
+
+console.log(a,b)
+
+let soma = a + b;
+
+console.log(soma)
